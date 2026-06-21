9ed259c20990ea2607f76e8096d26744a44ba8a3
liamtkg <ygj9f7v44y@privaterelay.appleid.com>
2026-06-21 11:48:52 +0100

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,1 +1,41 @@
+const STORAGE_KEY = "wifi_heatmap_survey";
 
+
+function saveData(points) {
+
+    localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(points)
+    );
+
+}
+
+
+
+function loadData() {
+
+    let data = localStorage.getItem(
+        STORAGE_KEY
+    );
+
+
+    if (data) {
+
+        return JSON.parse(data);
+
+    }
+
+
+    return [];
+
+}
+
+
+
+function deleteData() {
+
+    localStorage.removeItem(
+        STORAGE_KEY
+    );
+
+}
